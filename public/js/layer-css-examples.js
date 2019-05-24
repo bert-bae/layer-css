4bffa7b19e22dfb1599aef8ac27924d923a21d42
Elbert Bae <elbert.bae@gmail.com>
2019-05-23 23:12:31 -0700

    added custom overlay, shadow, and gradient examples

--- public/js/layer-css-examples.js
+++ public/js/layer-css-examples.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
-  
+  overlay('.custom-overlay-1', { color: "red", opacity: 0.3 });
+  overlay('.custom-overlay-2', { color: "green", opacity: 0.6 });
+
+  gradientL('.custom-gradient-1', { direction: "to right", from: "red", to: "transparent" });
+  gradientL('.custom-gradient-2', { direction: "45deg", from: "transparent", to: "blue" });
+
+  shadow('.custom-shadow-1', { right: "-10", bottom: "10", blur: "3", spread: "5", color: "rgba(255, 0, 0, 0.3)"});
+  shadow('.custom-shadow-2', { right: "10", bottom: "10", blur: null, spread: "2", color: "rgba(255, 200, 0, 0.6)"});
 })
--- public/js/layer-css.js
+++ public/js/layer-css.js
@@ -1,14 +1,13 @@
 function overlay(targetSelector, options) {
   // Options by default are Black with 0.5 opacity...
   // Options = { color, opacity }
-  let target = $(targetSelector);
-  target.addClass('.overlay');
+  $(targetSelector).addClass('overlay').append($('<div class="overlay-apply">'));
   if (options) {
     if (options.color) {
-      $(`${target}.overlay:before`).css('background-color', options.color);
+      $(`${targetSelector}.overlay .overlay-apply`).css('background-color', options.color);
     }
     if (options.opacity) {
-      $(`${target}.overlay:before`).css('opacity', options.opacity);
+      $(`${targetSelector}.overlay .overlay-apply`).css('opacity', options.opacity);
     }
   }
 }
@@ -18,14 +17,13 @@ function overlay(targetSelector, options) {
 // overlay('.overlay-example'); ... Will apply a overlay of black @ 50%
 // overlay('.overlay-example', { color: 'white', opacity: '0.3' }); ... Will apply a overlay of white @ 30%
 
-function gradient(targetSelector, options) {
+function gradientL(targetSelector, options) {
   // Default options... gradient goes from left to right transitioning from white to transparent
   // If options are present, all three values must be valid
-  // Options = { direction, from, to }
-  let target = $(targetSelector);
-  target.addClass('.gradient');
+  // Options = { direction, from, to }... Direction can be "to right", "to bottom", "90deg", etc.
+  $(targetSelector).addClass('gradientL').append($('<div class="gradientL-apply">'));
   if (options && options.direction && options.from && options.to) {
-    $(`${target}.gradient:before`).css('linear-gradient', `${options.drection} ${options.from}, ${options.to}`);  
+    $(`${targetSelector}.gradientL .gradientL-apply`).css('background-image', `linear-gradient(${options.direction}, ${options.from}, ${options.to})`);  
   }
 }
 
@@ -39,9 +37,8 @@ function shadow(targetSelector, options) {
   // If options are present, all values must be valid
   // Positive values for 1 & 2 arguments are bottom and right... negative numbers will go in opposite direction
   // Options = { bottom, right, range, color }... For color, use rgba if you want to apply opacity
-  let target = $(targetSelector);
-  target.addClass('.shadow');
-  if (options && options.bottom && options.right && options.range && options.color) {
-    $(`${target}.shadow:before`).css('box-shadow', `${options.bottom} ${options.right} ${options.range} ${options.color}`);
+  $(targetSelector).addClass('shadow').append($('<div class="shadow-apply">'));
+  if (options) {
+    $(`${targetSelector}.shadow .shadow-apply`).css('box-shadow', `${options.right || 0}px ${options.bottom || 0}px ${options.blur || 0}px ${options.spread || 0}px ${options.color || "#FFF"}`);
   }
 }
